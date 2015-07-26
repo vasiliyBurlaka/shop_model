c77a5ff7b696c1e37a95794d72160843ef09cc14
Vasiliy Burlaka <vasiliy.burlaka@gmail.com>
2015-07-27 00:35:41 +0300

    upd

--- spec/models/customer_spec.rb
+++ spec/models/customer_spec.rb
@@ -1,12 +1,6 @@
 require 'rails_helper'
 
 RSpec.describe Customer, type: :model do
-  pending "Should contain email, password, firstname, lastname
-    Email, password, firstname, lastname fields should be required
-    Email should be unique
-    Should have many orders, ratings
-    A customer should be able to create a new order
-    A customer should be able to return a current order in progress"
     let(:customer) {FactoryGirl.create :customer}
 
     it {expect(customer).to validate_presence_of(:email)}
@@ -18,5 +12,8 @@ RSpec.describe Customer, type: :model do
     
     it {expect(customer).to have_many(:order)}
     it {expect(customer).to have_many(:rating)}
+
+    pending "A customer should be able to create a new order
+      A customer should be able to return a current order in progress"
     
 end
